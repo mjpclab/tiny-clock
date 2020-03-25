5ee210a08956be661da8bb5e08300ca8f9771161
marjune <marjune@163.com>
2020-03-25 17:23:59 +0800

    feat: add shortcut key

--- main.pas
+++ main.pas
@@ -4,14 +4,16 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, ExtCtrls;
+  ExtCtrls, StdCtrls  ;
 
 type
   TForm1 = class(TForm)
-    Timer1: TTimer;
+    timer: TTimer;
     lblTime: TLabel;
-    procedure Timer1Timer(Sender: TObject);
+    procedure timerTimer(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure FormKeyDown(Sender: TObject; var Key: Word;
+      Shift: TShiftState);
   private
     { Private declarations }
     procedure UpdateTime();
@@ -31,7 +33,7 @@ begin
   lblTime.Caption:=FormatDateTime(' hh:mm:ss ', Now());
 end;
 
-procedure TForm1.Timer1Timer(Sender: TObject);
+procedure TForm1.timerTimer(Sender: TObject);
 begin
   UpdateTime();
 end;
@@ -40,6 +42,38 @@ procedure TForm1.FormCreate(Sender: TObject);
 begin
   self.DoubleBuffered:=true;
   UpdateTime();
+end;
+
+procedure TForm1.FormKeyDown(Sender: TObject; var Key: Word;
+  Shift: TShiftState);
+var
+  step:integer;
+  value:integer;
+begin
+  case Key of
+    27: // ESC
+      self.Close();
+    37: // left
+      begin
+        if self.AlphaBlendValue >=5 then
+          self.AlphaBlendValue:=self.AlphaBlendValue-5
+        else
+          self.AlphaBlendValue:=0
+      end;
+    39: // right
+      begin
+        if self.AlphaBlendValue <=250 then
+          self.AlphaBlendValue:=self.AlphaBlendValue+5
+        else
+          self.AlphaBlendValue:=255
+      end;
+    38: // up
+        lblTime.Font.Size:=lblTime.Font.Size+1;
+    40: // down
+      if lblTime.Font.Size > 5 then
+        lblTime.Font.Size:=lblTime.Font.Size-1;
+  end;
+
 end;
 
 end.
