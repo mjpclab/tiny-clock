9681d75e170942b8a6f6f7943b559e4e08d99cbd
marjune <marjune@163.com>
2020-09-28 23:53:08 +0800

    feat: make application single instance

--- clock.dpr
+++ clock.dpr
@@ -1,14 +1,21 @@
 program clock;
 
 uses
-  Forms,
+  Forms, Windows,
   main in 'main.pas' {frmMain},
   timeMan in 'timeMan.pas',
   bar in 'bar.pas' {frmBar};
 
 {$R *.res}
+var
+  Mutex: THandle;
 
 begin
+  Mutex := CreateMutex (nil, True, 'net.mjpclab.win_top_bar_clock');
+  if (Mutex = 0) or (GetLastError = ERROR_ALREADY_EXISTS) then begin
+    Exit;
+  end;
+
   Application.Initialize;
   Application.CreateForm(TfrmBar, frmBar);
   Application.Run;
